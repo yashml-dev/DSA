f5a9925b86c9e1947b0f99e97384f34f4d87724b
Yash Malviya <yashmalviya8689@gmail.com>
2026-07-13 11:04:45 +0530

    Time: 15 ms (86.64%), Space: 12.5 MB (85.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        freq1 = {}
+        freq2 = {}
+        for ch in s:
+            freq1[ch] = freq1.get(ch, 0) + 1
+        for ch in t:
+            freq2[ch] = freq2.get(ch, 0) + 1
+        return freq1 == freq2
+    
+        
