c1be6a4eb1723a60632e93548124d46c5d89e1aa
Yash Malviya <yashmalviya8689@gmail.com>
2026-07-13 11:05:25 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.11%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
+        
+        
